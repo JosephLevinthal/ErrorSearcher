527d4c758ab9bd181b216cc4e62a95187444c384
Joseph Levinthal <jvlo@icomp.ufam.edu.br>
2020-06-07 12:14:57 -0400

    Finish feedback analysis

--- docs/error_searcher.py
+++ docs/error_searcher.py
@@ -11,7 +11,8 @@ id_traducao = [
     '1165', '2171', '3119', '3135', '3396', '3555', '3812', '3851', '3960', '3985', '3986', '3987', '3992', '3995', '3999', '4001', '4002', '4004', '4008', '4011', '4012', '4013', '4014', '4057', '4058', '4059', '4061', '4066', '4067', '4073', '4078', '4079', '4080', '4124', '4125', '4126', '4128', '4130', '4131', '4132', '4134', '4135', '4143', '4146', '4150', '4153', '4154', '4157', '4162', '4163', '4165', '4170', '4171', '4173', '4174', '4177', '4178', '4181', '4184', '4185', '4187', '4192', '4193', '4194', '4271', '4310', '4322', '4342', '4345', '4347', '4349', '4351', '4352', '4358', '4366', '4367', '4372', '4413', '4414', '4422', '4424', '4427', '4429', '4431', '4432', '4443', '4464', '4469', '4470', '4472', '4477', '4479', '4481', '4482', '4485', '4487', '4501',
 ]
 
-my_file = 'error_samples.txt'
+my_file = 'saida.txt'
+time_final_da_dica = 0
 time_soma_total = 0
 lista_traducao = []
 
@@ -57,7 +58,6 @@ ZeroDivisionErrorFloat_quantidade = 0
 ZeroDivisionErrorFloat_tempo = 0
 # --------------------------------------
 
-
 with open(my_file, 'r') as f:
     for line in f:
         if(line.startswith("sequencia")):
@@ -71,76 +71,144 @@ with open(my_file, 'r') as f:
                 time_open = line.split('#')[0]
                 time_open_certo = time_open.split(" ")[3]
                 time_open_certo = datetime.strptime(time_open_certo, fmt)
-                # print(erro)
-                # print(time_open_certo)
+
+                if(time_final_da_dica > 0):
+                    time_soma_total = time_soma_total + time_final_da_dica
+                    print("Tempo de leitura dessa dica: ", time_final_da_dica)
+
+                    if('SyntaxError: invalid syntax' in line):
+                        SyntaxErrorInvalid_quantidade = SyntaxErrorInvalid_quantidade + 1
+                        SyntaxErrorInvalid_tempo = SyntaxErrorInvalid_tempo + time_final_da_dica
+
+                    elif('NameError: name' in line):
+                        NameError_quantidade = NameError_quantidade + 1
+                        NameError_tempo = NameError_tempo + time_final_da_dica
+
+                    elif('SyntaxError: unexpected EOF while parsing' in line):
+                        SyntaxErrorUnexpected_quantidade = SyntaxErrorUnexpected_quantidade + 1
+                        SyntaxErrorUnexpected_tempo = SyntaxErrorUnexpected_tempo + time_final_da_dica
+
+                    elif('IndentationError: expected an indented block' in line):
+                        IndentationErrorBlock_quantidade = IndentationErrorBlock_quantidade + 1
+                        IndentationErrorBlock_tempo = IndentationErrorBlock_tempo + time_final_da_dica
+
+                    elif('IndentationError: unexpected indent' in line):
+                        IndentationErrorUnexpected_quantidade = IndentationErrorUnexpected_quantidade + 1
+                        IndentationErrorUnexpected_tempo = IndentationErrorUnexpected_tempo + time_final_da_dica
+
+                    elif('IndentationError: unindent' in line):
+                        IndentationErrorUnindent_quantidade = IndentationErrorUnindent_quantidade + 1
+                        IndentationErrorUnindent_tempo = IndentationErrorUnindent_tempo + time_final_da_dica
+
+                    elif('TabError: inconsistent use' in line):
+                        TabErrorInconsistent_quantidade = TabErrorInconsistent_quantidade + 1
+                        TabErrorInconsistent_tempo = TabErrorInconsistent_tempo + time_final_da_dica
+
+                    elif('ValueError: invalid literal for int() with base 10' in line):
+                        ValueErrorInvalid_quantidade = ValueErrorInvalid_quantidade + 1
+                        ValueErrorInvalid_tempo = ValueErrorInvalid_tempo + time_final_da_dica
+
+                    elif('TypeError: unsupported operand type' in line):
+                        TypeErrorUnsuported_quantidade = TypeErrorUnsuported_quantidade + 1
+                        TypeErrorUnsuported_tempo = TypeErrorUnsuported_tempo + time_final_da_dica
+
+                    elif('TypeError: can' in line):
+                        TypeErrorCant_quantidade = TypeErrorCant_quantidade + 1
+                        TypeErrorCant_tempo = TypeErrorCant_tempo + time_final_da_dica
+
+                    elif('IndexError: index' in line):
+                        IndexErrorIndex_quantidade = IndexErrorIndex_quantidade + 1
+                        IndexErrorIndex_tempo = IndexErrorIndex_tempo + time_final_da_dica
+
+                    elif('ZeroDivisionError: division by zero' in line):
+                        ZeroDivisionErrorDivision_quantidade = ZeroDivisionErrorDivision_quantidade + 1
+                        ZeroDivisionErrorDivision_tempo = ZeroDivisionErrorDivision_tempo + time_final_da_dica
+
+                    elif('ZeroDivisionError: float division by zero' in line):
+                        ZeroDivisionErrorFloat_quantidade = ZeroDivisionErrorFloat_quantidade + 1
+                        ZeroDivisionErrorFloat_tempo = ZeroDivisionErrorFloat_tempo + time_final_da_dica
+
+                    time_final_da_dica = 0
 
             elif 'dica_close' in line:
                 time_close = line.split('#')[0]
                 time_close_certo = time_close.split(" ")[3]
                 time_close_certo = datetime.strptime(time_close_certo, fmt)
-                time_total_parcial = (time_close_certo -
-                                      time_open_certo).total_seconds()
-                # print(time_close_certo)
-                print("###", time_total_parcial)
-
-                if(time_total_parcial > 0):
-                    time_soma_total = time_soma_total + time_total_parcial
-                    if('SyntaxError: invalid syntax' in line):
-                        SyntaxErrorInvalid_quantidade = SyntaxErrorInvalid_quantidade + 1
-                        SyntaxErrorInvalid_tempo = SyntaxErrorInvalid_tempo + time_total_parcial
-
-                    elif('NameError: name' in line):
-                        NameError_quantidade = NameError_quantidade + 1
-                        NameError_tempo = NameError_tempo + time_total_parcial
-
-                    elif('SyntaxError: unexpected EOF while parsing' in line):
-                        SyntaxErrorUnexpected_quantidade = SyntaxErrorUnexpected_quantidade + 1
-                        SyntaxErrorUnexpected_tempo = SyntaxErrorUnexpected_tempo + time_total_parcial
-
-                    elif('IndentationError: expected an indented block' in line):
-                        IndentationErrorBlock_quantidade = IndentationErrorBlock_quantidade + 1
-                        IndentationErrorBlock_tempo = IndentationErrorBlock_tempo + time_total_parcial
-
-                    elif('IndentationError: unexpected indent' in line):
-                        IndentationErrorUnexpected_quantidade = IndentationErrorUnexpected_quantidade + 1
-                        IndentationErrorUnexpected_tempo = IndentationErrorUnexpected_tempo + time_total_parcial
-
-                    elif('IndentationError: unindent' in line):
-                        IndentationErrorUnindent_quantidade = IndentationErrorUnindent_quantidade + 1
-                        IndentationErrorUnindent_tempo = IndentationErrorUnindent_tempo + time_total_parcial
-
-                    elif('TabError: inconsistent use' in line):
-                        TabErrorInconsistent_quantidade = TabErrorInconsistent_quantidade + 1
-                        TabErrorInconsistent_tempo = TabErrorInconsistent_tempo + time_total_parcial
-
-                    elif('ValueError: invalid literal for int() with base 10' in line):
-                        ValueErrorInvalid_quantidade = ValueErrorInvalid_quantidade + 1
-                        ValueErrorInvalid_tempo = ValueErrorInvalid_tempo + time_total_parcial
-
-                    elif('TypeError: unsupported operand type' in line):
-                        TypeErrorUnsuported_quantidade = TypeErrorUnsuported_quantidade + 1
-                        TypeErrorUnsuported_tempo = TypeErrorUnsuported_tempo + time_total_parcial
-
-                    elif('TypeError: can' in line):
-                        TypeErrorCant_quantidade = TypeErrorCant_quantidade + 1
-                        TypeErrorCant_tempo = TypeErrorCant_tempo + time_total_parcial
-
-                    elif('IndexError: index' in line):
-                        IndexErrorIndex_quantidade = IndexErrorIndex_quantidade + 1
-                        IndexErrorIndex_tempo = IndexErrorIndex_tempo + time_total_parcial
-
-                    elif('ZeroDivisionError: division by zero' in line):
-                        ZeroDivisionErrorDivision_quantidade = ZeroDivisionErrorDivision_quantidade + 1
-                        ZeroDivisionErrorDivision_tempo = ZeroDivisionErrorDivision_tempo + time_total_parcial
-
-                    elif('ZeroDivisionError: float division by zero' in line):
-                        ZeroDivisionErrorFloat_quantidade = ZeroDivisionErrorFloat_quantidade + 1
-                        ZeroDivisionErrorFloat_tempo = ZeroDivisionErrorFloat_tempo + time_total_parcial
-
-                print(time_soma_total)
+                time_final_da_dica = (
+                    time_close_certo - time_open_certo).total_seconds()
 
             a = line.split()
             lista_traducao.append(a)
+
+    print("Soma total do tempo das dicas: ", time_soma_total)
+    print('\n')
+
+
+# Prints para o relatório final dos tempos
+    print("Número de Erros: SyntaxErrorInvalid: ",
+          SyntaxErrorInvalid_quantidade)
+    print("SyntaxErrorInvalid_tempo: ",
+          SyntaxErrorInvalid_tempo)
+    print('\n')
+
+    print("Número de Erros: NameError_quantidade: ", NameError_quantidade)
+    print("NameError_tempo: ", NameError_tempo)
+    print('\n')
+
+    print("Número de Erros: SyntaxErrorUnexpected: ",
+          SyntaxErrorUnexpected_quantidade)
+    print("SyntaxErrorUnexpected_tempo: ", SyntaxErrorUnexpected_tempo)
+    print('\n')
+
+    print("Número de Erros: IndentationErrorBlock: ",
+          IndentationErrorBlock_quantidade)
+    print("IndentationErrorBlock_tempo: ", IndentationErrorBlock_tempo)
+    print('\n')
+
+    print("Número de Erros: IndentationErrorUnexpected: ",
+          IndentationErrorUnexpected_quantidade)
+    print("IndentationErrorUnexpected_tempo: ",
+          IndentationErrorUnexpected_tempo)
+    print('\n')
+
+    print("Número de Erros: IndentationErrorUnindent: ",
+          IndentationErrorUnindent_quantidade)
+    print("IndentationErrorUnindent_tempo: ", IndentationErrorUnindent_tempo)
+    print('\n')
+
+    print("Número de Erros: TabErrorInconsistent: ",
+          TabErrorInconsistent_quantidade)
+    print("TabErrorInconsistent_tempo: ", TabErrorInconsistent_tempo)
+    print('\n')
+
+    print("Número de Erros: ValueErrorInvalid: ",
+          ValueErrorInvalid_quantidade)
+    print("ValueErrorInvalid_tempo: ", ValueErrorInvalid_tempo)
+    print('\n')
+
+    print("Número de Erros: TypeErrorUnsuported: ",
+          TypeErrorUnsuported_quantidade)
+    print("TypeErrorUnsuported_tempo: ", TypeErrorUnsuported_tempo)
+    print('\n')
+
+    print("Número de Erros: TypeErrorCant: ", TypeErrorCant_quantidade)
+    print("TypeErrorCant_tempo: ", TypeErrorCant_tempo)
+    print('\n')
+
+    print("Número de Erros: IndexErrorIndex: ",
+          IndexErrorIndex_quantidade)
+    print("IndexErrorIndex_tempo: ", IndexErrorIndex_tempo)
+    print('\n')
+
+    print("Número de Erros: ZeroDivisionErrorDivision: ",
+          ZeroDivisionErrorDivision_quantidade)
+    print("ZeroDivisionErrorDivision_tempo: ", ZeroDivisionErrorDivision_tempo)
+    print('\n')
+
+    print("Número de Erros: ZeroDivisionErrorFloat: ",
+          ZeroDivisionErrorFloat_quantidade)
+    print("ZeroDivisionErrorFloat_tempo: ", ZeroDivisionErrorFloat_tempo)
+    print('\n')
 
 with open('saida_traducao.txt', 'w') as f2:
     for item in lista_traducao:
